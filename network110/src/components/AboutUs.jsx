d6b6afb000284d758c56959da6868886b6fb0e32
fahadhassan8 <fahadfuuast2@gmail.com>
2024-07-02 09:39:22 -0700

    updated

--- network110/src/components/AboutUs.jsx
+++ network110/src/components/AboutUs.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "../css/aboutUs.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const AboutUs = () => {
   return (
     <div className="container about-us">
       <h1>About Us</h1>
       <div className="company-history">
-        <h2>Company History</h2>
+        <div className="company-history-img">
+          <img
+            src="https://img.freepik.com/free-vector/business-infographic-with-pic_23-2148334381.jpg?t=st=1719935902~exp=1719939502~hmac=f2939233b46441b58506ca7ce4d24f506e9ed240b17720d846ecf2253c7a0fb2&w=1060"
+            alt="company-history"
+          />
+        </div>
         <div className="company-history-wrapper">
+          <h2>Company History</h2>
           <p>
             Founded seven years ago, Network 110 has grown into a trusted name
             in internet, network, and surveillance solutions across the Kingdom
@@ -19,14 +27,8 @@ const AboutUs = () => {
             drive innovation and uphold the highest standards in every project
             we undertake.
           </p>
-          <img
-            src="https://img.freepik.com/free-vector/business-infographic-with-pic_23-2148334381.jpg?t=st=1719935902~exp=1719939502~hmac=f2939233b46441b58506ca7ce4d24f506e9ed240b17720d846ecf2253c7a0fb2&w=1060"
-            alt="company-history"
-          />
         </div>
       </div>
-
-      
     </div>
   );
 };
