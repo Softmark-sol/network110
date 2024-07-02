e2cbdb6ae7bf4e22dcb6915cafdb4e5fdd0a48f7
Rehman810 <rehmankp186@gmail.com>
2024-07-02 10:23:12 -0700

    hero done.

--- network110/src/pages/Home.jsx
+++ network110/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import AboutUs from "../components/AboutUs";
 import Cards from "../components/Cards";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Cards />
+      <Hero />
+      {/* <Cards /> */}
        {/* <AboutUs /> */}
     </div>
   );
