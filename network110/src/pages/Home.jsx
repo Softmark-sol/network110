f8d7c0913f2fc8af00234fee22e48a5f126ef27c
Rehman810 <rehmankp186@gmail.com>
2024-07-02 08:33:56 -0700

    basic structure ready.

---
+++ network110/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import AboutUs from '../components/aboutUs'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <AboutUs />
+    </div>
+  )
+}
+
+export default Home
