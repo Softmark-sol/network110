0e3e61451dea2f13534befdfa069aee99f76c6c8
Rehman810 <rehmankp186@gmail.com>
2024-07-02 10:52:47 -0700

    changes.

--- network110/src/components/Cards.jsx
+++ network110/src/components/Cards.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Img from "../assets/images/img.jpg"
+import "../css/cards.css"
 
 const Cards = () => {
   return (
-    <div>
-      
+    <div style={{width: "100%", display: "flex", justifyContent: "space-around", alignItems: "center", flexDirection: "row"}}>
+    <div className="cards">
+      <img src={Img} alt="img" />
+      <h2>Internet Cable Extension</h2>
+      <p>Hidden internal extension without breaking.</p>
+      <ul>
+        <li>Seamless extension</li>
+        <li>minimal disruption</li>
+      </ul>
     </div>
-  )
-}
+    {/* <div className="cards">
+      <img src={Img} alt="img" />
+      <h2>Internet Cable Extension</h2>
+      <p>Hidden internal extension without breaking.</p>
+      <ul>
+        <li>Seamless extension</li>
+        <li>minimal disruption</li>
+      </ul>
+    </div>
+    <div className="cards">
+      <img src={Img} alt="img" />
+      <h2>Internet Cable Extension</h2>
+      <p>Hidden internal extension without breaking.</p>
+      <ul>
+        <li>Seamless extension</li>
+        <li>minimal disruption</li>
+      </ul>
+    </div> */}
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
