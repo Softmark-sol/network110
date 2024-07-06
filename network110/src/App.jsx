9c2a761f3012adbc699d30b255508873f0b5139b
Najman <najmanmna@gmail.com>
2024-07-06 19:55:57 +0500

    bg particle effect animation added

--- network110/src/App.jsx
+++ network110/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import "animate.css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ParticleEffect from "./components/Particles";
 const App = () => {
   return (
     <div>
       <Home />
+      <ParticleEffect />
     </div>
   );
 };
