63509d6f918d036bc14fe4c790b1fe08707c983f
Rehman810 <rehmankp186@gmail.com>
2024-07-08 08:31:27 -0700

    changes

--- network110/src/components/Services.jsx
+++ network110/src/components/Services.jsx
@@ -6,11 +6,11 @@ import img2 from "../assets/images/img2.jpg";
 import img3 from "../assets/images/img3.jpg";
 import img4 from "../assets/images/img4.jpg";
 import img5 from "../assets/images/img5.jpg";
-import "../css/aboutUs.css";
 import img6 from "../assets/images/img6.jpg";
 import img7 from "../assets/images/img7.jpg";
 import img8 from "../assets/images/img8.jpg";
 import img9 from "../assets/images/img9.jpg";
+import "../css/aboutUs.css";
 
 const Services = () => {
   const { t } = useTranslation();
