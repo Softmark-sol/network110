885a736f360bd176525865554356fa4b69b8266b
Najman <najmanmna@gmail.com>
2024-07-10 16:48:33 +0500

    hero animation added

--- network110/src/components/Hero.jsx
+++ network110/src/components/Hero.jsx
@@ -1,28 +1,59 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import "../css/hero.css";
+import LogoParticles from "./ParticleLogo";
 
 const Hero = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  // Detect the current language
+  const currentLanguage = i18n.language;
 
   return (
     <div className="hero" id="hero">
       <div className="hero-container animate__animated animate__backInDown">
-        <ul>
-          <li>{t("hero.point1")}</li>
-          <li>{t("hero.point2")}</li>
-          <li>{t("hero.point3")}</li>
-          <li>{t("hero.point4")}</li>
-          <li>{t("hero.point5")}</li>
-          <li>{t("hero.point6")}</li>
-          <li>{t("hero.point7")}</li>
-          <li>{t("hero.point8")}</li>
-          <li>{t("hero.point9")}</li>
-          <li>{t("hero.point10")}</li>
-        </ul>
-        <div className="hero-btn">
-          <li>{t("hero.point11")}</li>
+        <div>
+          <ul>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point1")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point2")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point3")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point4")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point5")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point6")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point7")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point8")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point9")}
+            </li>
+            <li className={currentLanguage === "en" ? "english-text" : ""}>
+              {t("hero.point10")}
+            </li>
+          </ul>
         </div>
+        <div className="hero-btn">
+          <li className={currentLanguage === "en" ? "english-text" : ""}>
+            {t("hero.point11")}
+          </li>
+        </div>
+      </div>
+      <div className="logo-animation">
+        <LogoParticles />
       </div>
     </div>
   );
---
+++ network110/src/components/ParticleLogo.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef } from "react";
+import logo from "../assets/images/Network0.png";
+import "../css/particleLogo.css";
+
+const LogoParticles = () => {
+  const logoRef = useRef(null);
+  const containerRef = useRef(null);
+  const nextParticleRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://nextparticle.nextco.de/nextparticle.min.js";
+    script.onload = () => {
+      const container = containerRef.current;
+      nextParticleRef.current = new window.NextParticle({
+        image: logoRef.current,
+        width: container.clientWidth,
+        height: container.clientHeight,
+        particleGap: 1,
+      });
+
+      window.addEventListener("resize", handleResize);
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    if (nextParticleRef.current) {
+      const container = containerRef.current;
+      const width = container.clientWidth;
+      const height = container.clientHeight;
+      nextParticleRef.current.width = width;
+      nextParticleRef.current.height = height;
+      nextParticleRef.current.start();
+    }
+  };
+
+  return (
+    <div
+      className="particle-container"
+      ref={containerRef}
+      style={styles.container}
+    >
+      <img
+        src={logo}
+        ref={logoRef}
+        alt="Logo"
+        style={styles.logo}
+        onLoad={() => {
+          if (logoRef.current) {
+            logoRef.current.style.display = "block";
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  logo: {
+    display: "none",
+  },
+};
+
+export default LogoParticles;
